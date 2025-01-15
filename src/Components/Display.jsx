c490526fe86e8c8fc6e06b1ccb8fcfec14c537de
devabhisheksinghrautela <abhisheksinghrautela01@gmail.com>
2025-01-15 15:24:09 +0530

    add Songs Items, Album Item Modifed Display Home, Display featues in the project

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,13 +1,15 @@
 
 import DisplayHome from './DisplayHome'
-import { Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 const Display = () => {
   return (
     <div className= 'w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w[75%] lg:ml-0'>
+     <Routes>
      <Route>
      <Route path='/' element={<DisplayHome/>} />
      </Route>
+     </Routes>
     </div>
   )
 }
