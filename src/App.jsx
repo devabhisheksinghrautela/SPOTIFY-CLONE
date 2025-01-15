8ddbe9050296f29b8ff0db701fd05fa7ae9040fe
devabhisheksinghrautela <abhisheksinghrautela01@gmail.com>
2025-01-15 11:14:04 +0530

    Added Display Navbar DisplayHome and modifies Sidebar App and Main React Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Player from './Components/Player'
 import Sidebar from './Components/Sidebar'
+import Display from './Components/Display'
 
 const App = () => {
   return (
     <div className= 'h-screen bg-black'>
       <div className= 'h-[90%] flex'>
         <Sidebar />
+        <Display/>
 
       </div>
       
